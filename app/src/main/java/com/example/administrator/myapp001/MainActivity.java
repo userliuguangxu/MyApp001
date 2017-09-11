4b25eb98686eeb2d05d6524eeacf912cf4b732ff
userliuguangxu <814624239@qq.com>
2017-09-11 17:15:32 +0800

    test测试项目

--- app/src/main/java/com/example/administrator/myapp001/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapp001/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.administrator.myapp001;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView idd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//dsadsa
+        initView();
+        init();
+    }
+
+  private void init() {
+        idd.setText("张三丰");
+
+    }
+
+    private void initView() {
+        idd = (TextView) findViewById(R.id.idd);
     }
 }
